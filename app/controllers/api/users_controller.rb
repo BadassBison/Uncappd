33cd2a625ca8e8c6359062ce14509ab6b0feddb7
BadassBison <shawn.scott.xd@gmail.com>
2019-04-30 15:15:39 -0700

    Finish user/session controllers

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,33 @@
+class Api::UsersController < ApplicationController
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+          login!(@user)
+          render :show
+        else
+          render json: @user.errors.full_messages, status: 401
+        end
+      end
+      
+      def update
+        @user = User.find(params[:id])
+        if @user && @user.update(user_params)
+          render :show
+        elsif !@user
+          render json: ['Could not locate user'], status: 400
+        else
+          render json: @user.errors.full_messages, status: 401
+        end
+      end
+      
+      def show
+        @user = User.find(params[:id])
+      end
+
+      private
+      def user_params
+        params.require(:user).permit(:username, :email, :password)
+      end
+
+end
